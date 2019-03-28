8e8e4207fe70cab6881dd9489d756c89855d9fb1
wafaainina <wafa_ainina@apps.ipb.ac.id>
2019-03-28 14:27:19 +0700

    calendar view

--- app/src/main/java/com/psbo/cowplan/MainActivity.java
+++ app/src/main/java/com/psbo/cowplan/MainActivity.java
@@ -1,13 +1,46 @@
 package com.psbo.cowplan;
 
+import android.app.usage.UsageEvents;
+import android.arch.lifecycle.Lifecycle;
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    CalendarView calendarView;
+    TextView myDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        myDate = (TextView) findViewById(R.id.mydate);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int dayOfMonth, int month, int year) {
+                String date = (dayOfMonth + 1) + "/" + month + "/" + year;
+                myDate.setText(date);
+
+            }
+        }
+
     }
 }
