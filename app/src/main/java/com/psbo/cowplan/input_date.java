d8c42d9042a82b17c603b472a5d3b1b9bcab6631
wafaainina <wafa_ainina@apps.ipb.ac.id>
2019-04-07 18:16:06 +0700

    ganti radio button jadi button

--- app/src/main/java/com/psbo/cowplan/input_date.java
+++ app/src/main/java/com/psbo/cowplan/input_date.java
@@ -1,27 +1,22 @@
 package com.psbo.cowplan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class input_date extends AppCompatActivity {
-    RadioGroup rg;
-    RadioButton rb;
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_date);
-    rg = (RadioGroup) findViewById(R.id.rgroup);
-
     }
+
 }
 
-public void rbclick(View v){
-    int radiobuttonid = rg.getCheckedRadioButtonId();
-    rb = (RadioButton) findViewById(radiobuttonid);
-
-    Toast.makeText(getBaseContext(), rb.getText(),Toast.LENGTH_LONG).show();
-}
